52edd5e3a8d68a1e44978303faf22e5ed85e8649
louisguitton <louisguitton93@gmail.com>
2017-11-04 17:58:15 +0100

    added doc links

--- start_uswgi_service.sh
+++ start_uswgi_service.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# https://www.digitalocean.com/community/tutorials/how-to-serve-flask-applications-with-uwsgi-and-nginx-on-ubuntu-16-04
 cp ~/site/site.service /etc/systemd/system/site.service
 sudo systemctl start site
 sudo systemctl enable site
