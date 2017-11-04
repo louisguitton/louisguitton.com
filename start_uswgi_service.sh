2a7cfab9fb26fbe93c235f4ce33dde9e108b9d7d
louisguitton <louisguitton93@gmail.com>
2017-11-04 17:53:33 +0100

    configuring nginx

--- start_uswgi_service.sh
+++ start_uswgi_service.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
-cp site/site.service /etc/systemd/system/site.service
+cp ~/site/site.service /etc/systemd/system/site.service
 sudo systemctl start site
 sudo systemctl enable site
+
+cp ~/site/nginx_config /etc/nginx/sites-available/site
+sudo ln -s /etc/nginx/sites-available/site /etc/nginx/sites-enabled
