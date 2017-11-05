b68abad3b3f40a663af3b50e69372530424d0451
louisguitton <louisguitton93@gmail.com>
2017-11-05 21:50:03 +0100

    reworked navigation

--- app/views.py
+++ app/views.py
@@ -7,37 +7,14 @@ from .forms import LoginForm
 @app.route('/')
 @app.route('/index')
 def index():
-
-    projects = [  # fake array of projects
-        {
-            'title': "2055 = 2048 + 007",
-            'description': "The 2048 game meets James Bond!",
-            'nickname': '2055',
-            'template': "2055.html"
-        },
-        {
-            'title': "Football Betting",
-            'description': "A dashboard of the SoFootLigue Data.",
-            'nickname': 'sofoot',
-            'template': "sofoot.html"
-        },
-        {
-            'title': "songGIFy",
-            'description': "An automatic GIF video clip for the song of my band.",
-            'nickname': 'songgify',
-            'template': "songgify.html"
-        }
-    ]
-
     return render_template(
         "index.html",
         title='Home',
-        projects=projects
     )
 
 
-@app.route('/contact')
-def contact():
+@app.route('/about')
+def about():
     social_links = [
         {
             "title": "linkedin",
@@ -53,27 +30,15 @@ def contact():
         }
     ]
     return render_template(
-        "contact.html",
-        title='Contact',
+        "about.html",
+        title='About',
         social_links=social_links
     )
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    return render_template('login.html',
-                           title='Sign In',
-                           form=form)
-
-
+@app.route('/projects')
 @app.route('/projects/<nickname>')
-def projects(nickname):
-
+def projects(nickname=None):
     projects = [  # fake array of projects
         {
             'title': "2055 = 2048 + 007",
@@ -94,10 +59,30 @@ def projects(nickname):
             'template': "songgify.html"
         }
     ]
-    selected_project = [p for p in projects if p['nickname'] == nickname][0]
 
-    return render_template(
-        'project.html',
-        title=nickname,
-        project=selected_project
-    )
+    if nickname:
+        selected_project = [p for p in projects if p['nickname'] == nickname][0]
+
+        return render_template(
+            'project.html',
+            title=nickname,
+            project=selected_project
+        )
+    else:
+        return render_template(
+            'projects.html',
+            title='Projects',
+            projects=projects
+        )
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
+    return render_template('login.html',
+                           title='Sign In',
+                           form=form)
